c0799b2c4ba2e6fcff28ad5a9c7d78bef9266f7c
FIROKS <firoks1@gmail.com>
2020-06-03 15:53:40 +0300

    Обновляет билд

---
+++ build/js/scripts.js
@@ -0,0 +1,18 @@
+function initMap() {
+  var uluru = {lat: 59.9385, lng: 30.3229};
+  var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 17,
+  disableDefaultUI: false,
+  center: uluru,
+  });
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    title: 'cat-energy',
+    icon: {
+      url: "../img/map-pin.png",
+      scaledSize: new google.maps.Size(113, 106)
+    }
+    });
+  }
